cae19853eda2436ffe5cb293e070c1839e25781c
Gihak111 <yeonjm12@gmail.com>
2024-06-05 13:07:49 +0900

    creat_right_sidebar

---
+++ assets/js/right_scroll_script.js
@@ -0,0 +1,19 @@
+// your-scroll-script.js
+
+// 스크롤을 따라오는 요소를 고정하는 함수
+function fixSidebar() {
+    var sidebar = document.getElementById('sidebar');
+    var sticky = sidebar.offsetTop;
+  
+    if (window.pageYOffset > sticky) {
+      sidebar.classList.add('fixed');
+    } else {
+      sidebar.classList.remove('fixed');
+    }
+  }
+  
+  // 스크롤 이벤트 리스너 등록
+  window.onscroll = function() {
+    fixSidebar();
+  };
+  
